ab1b37bcab5866b7cab470bfd68e1bf1d0ad652b
lidia.garstea <lidiagarshtea@gmail.com>
2022-02-10 19:08:01 +0200

    Commit intellij

---
+++ src/homework/lesson7/SummingTwoNumbers.java
@@ -0,0 +1,27 @@
+package homework.lesson7;
+
+import java.util.Scanner;
+
+public class SummingTwoNumbers {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        do {
+            System.out.println("Enther the first number:" + " ");
+            int number1 = input.nextInt();
+            System.out.println("Enter the second number" + " ");
+            int number2 = input.nextInt();
+            int sum = number1 + number2;
+            System.out.println("Sum of number is:" + " " + sum);
+            System.out.println("Your operation was succsseful!");
+            System.out.println("You want to perform the operation again? Indroduce yes/no:" + " ");
+            String stop = input.next(); {
+                if (stop.equals("no"))
+                    break;
+                continue;
+            }
+        }
+        while (true); {
+            System.out.println ("Your operation has ended! Thank you!");
+        }
+    }
+}
