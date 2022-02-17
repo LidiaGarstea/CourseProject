2cb641ac1817106fa5842a063dec3e02deb13b9b
lidia.garstea <lidiagarshtea@gmail.com>
2022-02-17 18:55:53 +0200

    Commit intellij

--- src/homework/lesson8/SumAverageArray.java
+++ src/homework/lesson8/SumAverageArray.java
@@ -6,18 +6,36 @@ public class SumAverageArray {
         int twoDimensionalArray [] [] = {{3,4,5,8 }, {4, 6, 8, 9}};
         int arraySum1 = 0;
         int arraySum2 = 0;
+        int numEven = 0;
+        int numOdd = 0;
 
-        for (int i = 0; i < oneDimensionalArray.length; i++)
+        for (int i = 0; i < oneDimensionalArray.length; i++) {
+                if (i % 2 == 0) {
+                    numOdd++;
+            }
+                else numEven++;
             arraySum1 += oneDimensionalArray[i];
+        }
         {
-            System.out.println("The sum of the elements of the one dimensional array is:" + " " + arraySum1);
-            System.out.println("The arithmetic mean of the one dimensional array is:" + " " + arraySum1 / oneDimensionalArray.length);
+            System.out.println("One dimensional array:");
+            System.out.println("The sum of the elements is:" + " " + arraySum1);
+            System.out.println("The arithmetic mean is:" + " " + arraySum1 / oneDimensionalArray.length);
+            System.out.println("The number off elements evens:" + " " + numEven);
+            System.out.println("The number off elements odd:" + " " + numOdd + "\n");
 
             for (int i = 0; i < twoDimensionalArray.length; i++) {
-                for (int j = 0; j < twoDimensionalArray.length; j++)
-                arraySum2 += twoDimensionalArray [i] [j]; }
-                System.out.println("The sum of the elements of the one dimensional array is:" + " " + arraySum2);
-                System.out.println("The arithmetic mean of the one dimensional array is:" + " " + arraySum2 / twoDimensionalArray.length);
+                int j;
+                for (j = 0; j < twoDimensionalArray.length; j++)
+                    if (i % 2 == 0 && j % 2 == 0) {
+                        numOdd++;
+                    } else numEven++;
+                arraySum2 += twoDimensionalArray[i][j];
+            }
+            System.out.println("Two dimensional array:");
+            System.out.println("The sum of the elements is:" + " " + arraySum2);
+            System.out.println("The arithmetic mean is:" + " " + arraySum2 / twoDimensionalArray.length);
+            System.out.println("The number off elements evens:" + " " + numEven);
+            System.out.println("The number off elements odd:" + " " + numOdd);
         }
     }
 }
